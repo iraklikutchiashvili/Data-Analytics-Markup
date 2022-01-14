1bc93be2a39fc4ac29ef6ad8f336cb863b15abc9
iraklikutchiashvili <irakli9823@gmail.com>
2022-01-14 17:35:47 +0400

    add js

---
+++ script.js
@@ -0,0 +1,12 @@
+let navigation = document.getElementById('links');
+let burgerButton = document.getElementById('toggleButton');
+burgerButton.addEventListener('click', function() {
+    navigation.classList.toggle('active');
+})
+
+document.getElementById('clickButton').addEventListener('click', function() {
+    alert('Congratulations')
+})
+document.getElementById('learnmoreButton').addEventListener('click', function() {
+    alert('Learned')
+})
